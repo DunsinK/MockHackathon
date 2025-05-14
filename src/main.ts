96e492603ecc8526c11a5cbe9eb75c68f7e3bd34
DunsinK <133840905+DunsinK@users.noreply.github.com>
2025-05-14 12:22:42 -0500

    Basic Information
    Named Hackathon U-Hack created image and removed filler Vite info.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <img src="./Assets/U-Hack_Photo.webp" style={{width:'300px'}}/>
+    <h1> U-Hack </h1>
+    <p> December 32-33, 2050 </p> 
+    
+    <h1> About us</h1>
+    <p> U-Hack is the ultimate intercollegiate hackathon — a weekend of innovation, creativity, and collaboration for students from universities everywhere. Whether you're building your first app or deploying machine learning models, U-Hack is your launchpad.</p>
+    
+    <h1> What to Expect </h1>
+    <li>48 hours of hacking, mentoring, and problem-solving </li>
+
+    <li> Workshops led by industry experts </li>
+
+    <li> Sponsor challenges with awesome prizes </li>
+
+    <li> Free swag, food, and tech tools to fuel your project </li>
+
+    <li> A chance to connect with recruiters and launch your ideas </li> 
+    
+    <h1> Who can Attend? </h1>
+    <p> University students of all backgrounds and majors. No experience? No problem — we welcome beginners, coders, designers, and dreamers alike! </p>
+
+    <h1> Why U-Hack? </h1>
+    <p> Because it’s more than a hackathon — it’s a movement. We bring universities together to collaborate, compete, and create real impact. </p>
+
+    <h1> Ready to hack? </h1>
+    <button> Apply Now </button>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
